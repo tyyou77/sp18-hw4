d9901b0a36692aa1d6afc7e3773b36e6f3fbac90
Charles You <cyou77@berkeley.edu>
2018-03-08 16:57:17 -0800

    :'(

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'home#home'
+    get 'root', to: 'home#home'
 end
+
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root 'students#new'
+#   get 'root', to: 'students#new'
+#   get '/teachers/new', to: 'teachers#new'
+#   post '/teachers', to: 'teachers#create'
+#   get  '/students/new', to: 'students#new'
+#   post '/students', to: 'students#create'
+# end
